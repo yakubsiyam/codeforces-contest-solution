3f966f55aa640c0996fd611e7bd2c70abb79d788
Siyam <67267877+yakubsiyam@users.noreply.github.com>
2022-12-03 15:18:39 +0600

    Doremy's Paint solution

---
+++ A. Doremy's Paint(global24).cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int n,v,l,r, cnt = 0;
+        vector<int> a;
+        vector<int> elem;
+        cin >> n;
+        for(int i = 0; i<n; i++)
+        {
+            cin >> v;
+            a.push_back(v);
+        }
+        cout << 1 << " " << n << "\n";
+    }
+//    main();
+    return 0;
+}
+
+
+
