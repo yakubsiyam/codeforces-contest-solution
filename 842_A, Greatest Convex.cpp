c936187533f6ab23d07fca7995d5238f4f099960
Siyam <67267877+yakubsiyam@users.noreply.github.com>
2023-01-06 14:57:15 +0600

    Greatest Convex code solution

---
+++ 842_A, Greatest Convex.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int T;
+    cin >> T;
+
+    while(T--)
+    {
+        int n;
+        cin >> n;
+
+        cout << n - 1 << endl;
+    }
+    return 0;
+}
