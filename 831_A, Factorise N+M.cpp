34c5026be18a89beef60c4ec61f68495dea30b5e
Siyam <67267877+yakubsiyam@users.noreply.github.com>
2023-01-06 14:56:11 +0600

    Factorise N+M code solution

---
+++ 831_A, Factorise N+M.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int T;
+    cin >> T;
+
+    while(T--)
+    {
+        int n;
+        cin >> n;
+
+        if(n == 2)
+            cout << "7" << endl;
+        else
+            cout << "3" << endl;
+    }
+
+    return 0;
+}
