6abcbe318ae7c043be7a1200698a56adfd593989
Siyam <67267877+yakubsiyam@users.noreply.github.com>
2023-01-10 13:25:09 +0600

    Print a Pedestal (Codeforces logo?) code solution

---
+++ 797_A, Print a Pedestal.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int T;
+    cin >> T;
+
+    while(T--)
+    {
+        double n;
+        int h1, h2, h3;
+        cin >> n;
+
+        h2 = ceil(n / 3);
+        h1 = h2 + 1;
+        h3 = n - (h1 + h2);
+        if(h3 == 0)
+        {
+            h2--;
+            h3++;
+        }
+        cout << h2 << " " << h1 << " " << h3 << endl;
+    }
+
+    return 0;
+}
