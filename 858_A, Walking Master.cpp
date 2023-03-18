b4b6060ea4cc5fd65864c3f0b6176d5fcae58568
Siyam <67267877+yakubsiyam@users.noreply.github.com>
2023-03-18 22:56:31 +0600

    "Walking Master" code solution

---
+++ 858_A, Walking Master.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+	
+    if (d < b) 
+    {
+      cout << "-1\n";
+      return;
+    }
+ 
+    int req = (d - b);
+    a += req;
+ 
+    if (c > a) 
+    {
+      cout << "-1\n";
+      return;
+    }
+ 
+    req += (a - c);
+    cout << req << "\n";
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int T;
+    cin >> T;
+    while(T--)
+    {
+        solve();
+    }
+    return 0;
+}
